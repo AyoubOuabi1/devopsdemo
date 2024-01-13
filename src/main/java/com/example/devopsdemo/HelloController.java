dbd603a10e7e36c7a630700078d1d4176026fcf1
ayoub ouabi <ayoub.ouabi06@gmail.com>
2024-01-13 15:55:11 +0100

    login worked and now working on build and push

--- src/main/java/com/example/devopsdemo/HelloController.java
+++ src/main/java/com/example/devopsdemo/HelloController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
+
+
     @GetMapping("")
    public String hello(){
         return "Hello World";
