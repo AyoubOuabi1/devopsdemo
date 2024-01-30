cbb515791b59f82a89af65c8631069413fc58c54
Ayoub Ouabi <112890204+AyoubOuabi1@users.noreply.github.com>
2024-01-30 09:39:07 +0100

    Update HelloController.java

--- src/main/java/com/example/devopsdemo/HelloController.java
+++ src/main/java/com/example/devopsdemo/HelloController.java
@@ -14,7 +14,7 @@ public class HelloController {
     }
     @GetMapping("/ayoub")
    public String hellop(){
-        return "Hello meeeeeee s";
+        return "Hello meeeeeee  njnmmm s";
     }
 
 
