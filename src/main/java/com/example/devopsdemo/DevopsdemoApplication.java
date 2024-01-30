8c67f8c63503b2942bfab855ce5937c842d823e4
Ayoub Ouabi <112890204+AyoubOuabi1@users.noreply.github.com>
2024-01-30 09:34:01 +0100

    Update DevopsdemoApplication.java

--- src/main/java/com/example/devopsdemo/DevopsdemoApplication.java
+++ src/main/java/com/example/devopsdemo/DevopsdemoApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DevopsdemoApplication {
 
 
+
+    
     public static void main(String[] args) {
         SpringApplication.run(DevopsdemoApplication.class, args);
     }
