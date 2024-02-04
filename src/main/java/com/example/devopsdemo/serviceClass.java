dbc04c0443a7027ca17a107925dcb0e0cf61a469
ayoub ouabi <ayoub.ouabi06@gmail.com>
2024-02-04 20:22:21 +0100

    working with sonar qube

--- src/main/java/com/example/devopsdemo/serviceClass.java
+++ src/main/java/com/example/devopsdemo/serviceClass.java
@@ -3,6 +3,41 @@ package com.example.devopsdemo;
 public class serviceClass {
 
     public static boolean isService(){
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
         return false;
     }
+    public static boolean isServices(){
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        return false;
+    }
+    public static boolean isServicess(){
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        return false;
+    }
+    public static boolean isServicesss(){
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        return false;
+    }
+    public static boolean isServicesssss(){
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        return false;
+    }
+
+
+
 }
